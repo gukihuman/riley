ed9eae8a76c5f97bd66b6ac73086bd51506a046d
gukihuman <sokolovivanf@gmail.com>
2025-02-01 17:07:51 +0500

    add textarea

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { init, h, classModule } from "snabbdom"
-const patch = init([classModule])
-const vnode = h("body.bg-sky-500", "Hello, Snabbdom.")
-const app = document.body
-patch(app, vnode)
+// import { init, h, classModule } from "snabbdom"
+// const patch = init([classModule])
+// const vnode = h("body.bg-sky-500", "Hello, Snabbdom.")
+// const app = document.body
+// patch(app, vnode)
